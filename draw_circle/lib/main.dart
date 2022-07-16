f6f5e7036e55c89ff5657ff2471939ccad6dee42
AhRastgar <ah.rastgar.c@gmail.com>
2022-07-16 13:11:10 +0430

    Draw circle

---
+++ draw_circle/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Draw Blue Circle',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Draw purple Circle'),
+      ),
+      body: ListView(children: <Widget>[
+        Container(
+          width: 400,
+          height: 400,
+          child: CustomPaint(
+            painter: OpenPainter(),
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+class OpenPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint1 = Paint()
+      ..color = Color.fromARGB(255, 79, 20, 155)
+      ..style = PaintingStyle.fill;
+    canvas.drawCircle(Offset(200, 200), 100, paint1);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
